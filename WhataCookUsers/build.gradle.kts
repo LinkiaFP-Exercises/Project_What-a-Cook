126d377c3041b71d2a87ba98fa3cfa94cfc050e2
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-02-23 13:08:05 +0100

    initial project and db config by env

---
+++ WhataCookUsers/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.whatacook"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web-services")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ WhataCookUsers/src/main/java/com/whatacook/cookers/WhataCookUsersApplication.java
@@ -0,0 +1,13 @@
+package com.whatacook.cookers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhataCookUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhataCookUsersApplication.class, args);
+    }
+
+}
