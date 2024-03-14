98acb3c65eef0d5f3d8ea67d43059c9a32f23293
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-03-14 12:13:54 +0100

    SetNewPasswordFlowHandler fail response fix

--- WhataCookUsers/src/main/java/com/whatacook/cookers/config/filter/implement/SetNewPasswordFlowHandlerImpl.java
+++ WhataCookUsers/src/main/java/com/whatacook/cookers/config/filter/implement/SetNewPasswordFlowHandlerImpl.java
@@ -3,7 +3,9 @@ package com.whatacook.cookers.config.filter.implement;
 import com.whatacook.cookers.config.filter.AuthenticationManager;
 import com.whatacook.cookers.config.filter.ResponseErrorHtml;
 import com.whatacook.cookers.config.filter.SetNewPasswordFlowHandler;
+import com.whatacook.cookers.model.constants.Htmls;
 import com.whatacook.cookers.service.ResetService;
+import com.whatacook.cookers.utilities.GlobalValues;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -14,6 +16,7 @@ import reactor.core.publisher.Mono;
 public class SetNewPasswordFlowHandlerImpl implements SetNewPasswordFlowHandler {
 
     private final ResetService resetService;
+    private final GlobalValues globalValues;
     private final AuthenticationManager authenticationManager;
     private final ResponseErrorHtml responseErrorHtml;
 
@@ -21,8 +24,11 @@ public class SetNewPasswordFlowHandlerImpl implements SetNewPasswordFlowHandler 
     @Override
     public Mono<Void> handle(String keyCodeToSet, ServerWebExchange exchange, WebFilterChain chain) {
         String codeToSet = exchange.getRequest().getQueryParams().getFirst(keyCodeToSet);
+        String FAIL_HTML_FOR_RESET = Htmls.FailSetNewPassword.get()
+                .replace("LOGO_WAC", globalValues.getUrlWacLogoPngSmall())
+                .replace("EMAIL_WAC", globalValues.getMailToWac());
         return resetService.findByCode(codeToSet)
                 .flatMap(resetDto -> authenticationManager.setAuthenticated(resetDto.getId(), null, exchange, chain))
-                .switchIfEmpty(Mono.defer(() -> responseErrorHtml.send(exchange, "<h1>Fallo en el filter!</h1>")));
+                .switchIfEmpty(Mono.defer(() -> responseErrorHtml.send(exchange, FAIL_HTML_FOR_RESET)));
     }
 }
