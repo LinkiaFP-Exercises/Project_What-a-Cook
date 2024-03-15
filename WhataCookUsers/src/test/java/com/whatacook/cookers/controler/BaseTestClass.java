99791064c805db7eb8855515c9d8f3df301ef184
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-03-15 09:06:17 +0100

    Update CheckEmailTest.java
    Create BaseTestClass.java

---
+++ WhataCookUsers/src/test/java/com/whatacook/cookers/controler/BaseTestClass.java
@@ -0,0 +1,96 @@
+package com.whatacook.cookers.controler;
+
+import com.whatacook.cookers.model.constants.AccountStatus;
+import com.whatacook.cookers.model.constants.Role;
+import com.whatacook.cookers.model.users.UserDTO;
+import com.whatacook.cookers.service.contracts.ActivationDao;
+import com.whatacook.cookers.service.contracts.ResetDao;
+import com.whatacook.cookers.service.contracts.UserDao;
+import org.assertj.core.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.MAP;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BaseTestClass {
+
+    @Autowired
+    protected WebTestClient webTestClient;
+
+    @MockBean
+    protected UserDao userDao;
+    @MockBean
+    protected ActivationDao activationDao;
+    @MockBean
+    protected ResetDao resetDaoDAO;
+    @MockBean
+    protected JavaMailSender mailSender;
+
+    protected static String requestBodyOnlyMail(String email) {
+        return "{ \"email\": \"" + email + "\" }";
+    }
+
+    protected static String requestBodyFull(String email, String password, String firstName, String surNames, String birthdate) {
+        return "{\n" +
+                "    \"email\": \"" + email + "\",\n" +
+                "    \"password\": \"" + password + "\",\n" +
+                "    \"firstName\": \"" + firstName + "\",\n" +
+                "    \"surNames\": \"" + surNames + "\",\n" +
+                "    \"birthdate\": \"" + birthdate + "\"\n" +
+                "}";
+    }
+
+    protected static String requestBodyFullyFill() {
+        return requestBodyFull(EMAIL, PASSWORD, FIRST_NAME, SUR_NAMES, BIRTHDATE_STR);
+    }
+
+    protected static UserDTO userDtoBasicPending() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.set_id(ID);
+        userDTO.setRegistration(LOCAL_DATE_TIME);
+        userDTO.setEmail(EMAIL);
+        userDTO.setPassword(PASSWORD);
+        userDTO.setFirstName(FIRST_NAME);
+        userDTO.setSurNames(SUR_NAMES);
+        userDTO.setBirthdate(BIRTHDATE);
+        userDTO.setRoleType(Role.BASIC);
+        userDTO.setAccountStatus(AccountStatus.PENDING);
+        return userDTO;
+    }
+    protected static UserDTO userDtoBasicOk() {
+        UserDTO userDTO = userDtoBasicPending();
+        userDTO.setAccountStatus(AccountStatus.OK);
+        return userDTO;
+    }
+
+    public static final String ID = "65db4a16e6cd946d5eb775fa";
+    public static final LocalDateTime LOCAL_DATE_TIME = LocalDateTime.of(2024, 1, 20, 16, 20);
+    public static final String EMAIL = "esgotilha@protonmail.ch";
+    public static final String PASSWORD = "Test!234";
+    public static final String FIRST_NAME = "Fulano";
+    public static final String SUR_NAMES = "Ciclano Beltrano";
+    public static final LocalDate BIRTHDATE = LocalDate.of(1982, 7, 19);
+    public static final String BIRTHDATE_STR = "1982-07-19";
+
+    void testFailRequest_400(String uri, String requestBody, boolean success, String message, String key) {
+        webTestClient.post().uri(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(requestBody)
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .jsonPath("$.success").isEqualTo(success)
+                .jsonPath("$.message").value(text -> Assertions.assertThat(text).asString().contains(message))
+                .jsonPath("$.content").value(content -> assertThat(content).asInstanceOf(MAP).containsKeys(key));
+    }
+
+}
--- WhataCookUsers/src/test/java/com/whatacook/cookers/controler/CheckEmailTest.java
+++ WhataCookUsers/src/test/java/com/whatacook/cookers/controler/CheckEmailTest.java
@@ -1,46 +1,25 @@
 package com.whatacook.cookers.controler;
 
-import com.whatacook.cookers.service.contracts.UserDao;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
-import java.util.Map;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.MAP;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class CheckEmailTest {
-
-    @Autowired
-    private WebTestClient webTestClient;
-
-    @MockBean
-    private UserDao DAO;
+public class CheckEmailTest extends BaseTestClass {
 
     @Value("${app.endpoint.users-check-email}")
     private String usersCheckEmailEndpoint;
 
-    private static String requestBody(String email) {
-        return "{ \"email\": \"" + email + "\" }";
-    }
-
     @BeforeEach
     void setUp() {
-        Mockito.when(DAO.existsByEmail(Mockito.anyString())).thenReturn(Mono.just(false));
+        Mockito.when(userDao.existsByEmail(Mockito.anyString())).thenReturn(Mono.just(false));
     }
 
     @ParameterizedTest
@@ -49,10 +28,10 @@ public class CheckEmailTest {
             "email@teste.com, false, User does not exist yet, false"
     })
     void testExistsByEmail_OK(String email, boolean exists, String message, boolean content) {
-        Mockito.when(DAO.existsByEmail(email)).thenReturn(Mono.just(exists));
+        Mockito.when(userDao.existsByEmail(email)).thenReturn(Mono.just(exists));
         webTestClient.post().uri(usersCheckEmailEndpoint)
                 .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(requestBody(email))
+                .bodyValue(requestBodyOnlyMail(email))
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody()
@@ -64,18 +43,7 @@ public class CheckEmailTest {
     @ParameterizedTest
     @MethodSource("provideVariablesForFailRequests")
     void testExistsByEmail_Fail(String requestBody, boolean success, String message, String key) {
-        webTestClient.post().uri(usersCheckEmailEndpoint)
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(requestBody)
-                .exchange()
-                .expectStatus().isBadRequest()
-                .expectBody()
-                .jsonPath("$.success").isEqualTo(success)
-                .jsonPath("$.message").value(text -> Assertions.assertThat(text).asString().contains(message))
-                .jsonPath("$.content").value(content -> {
-                    assertThat(content).isInstanceOf(Map.class);
-                    assertThat(content).asInstanceOf(MAP).containsKeys(key);
-                });
+        testFailRequest_400(usersCheckEmailEndpoint, requestBody, success, message, key);
     }
 
     private static Stream<Arguments> provideVariablesForFailRequests() {
@@ -85,9 +53,9 @@ public class CheckEmailTest {
         String mailKey = "email";
         String errorKey = "ERROR";
         return Stream.of(
-                Arguments.of(requestBody("email@ro ot.com"), false, messageMail, mailKey),
-                Arguments.of(requestBody(""), false, messageMail, mailKey),
-                Arguments.of(requestBody(null), false, messageMail, mailKey),
+                Arguments.of(requestBodyOnlyMail("email@ro ot.com"), false, messageMail, mailKey),
+                Arguments.of(requestBodyOnlyMail(""), false, messageMail, mailKey),
+                Arguments.of(requestBodyOnlyMail(null), false, messageMail, mailKey),
                 Arguments.of(badlyJson, false, messageBody, errorKey),
                 Arguments.of("", false, messageBody, errorKey)
         );
