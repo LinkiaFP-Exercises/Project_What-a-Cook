08e3a1a9c42c33d1991f95f08c43ef278b69a4f3
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-04-18 11:14:29 +0200

    Update AnyRequestFilter.java

--- WhataCookUsers/src/main/java/com/whatacook/cookers/config/filter/AnyRequestFilter.java
+++ WhataCookUsers/src/main/java/com/whatacook/cookers/config/filter/AnyRequestFilter.java
@@ -3,7 +3,6 @@ package com.whatacook.cookers.config.filter;
 import com.whatacook.cookers.config.jwt.JwtUtil;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -23,36 +22,34 @@ public class AnyRequestFilter implements WebFilter {
                             EmailResetPasswordFlowHandler emailResetPasswordFlowHandler,
                             SetNewPasswordFlowHandler setNewPasswordFlowHandler) {
         handlers = new HashMap<>();
-
-        handlers.put(jwtUtil.getHeader(), (exchange, chain, keys) -> tokenAuthenticationFlowHandler.handle(exchange, chain));
-        handlers.put(jwtUtil.getActivation(), (exchange, chain, keys) -> activationCodeFlowHandler.handle(keys[0], exchange, chain));
-        handlers.put(jwtUtil.getResend(), (exchange, chain, keys) -> emailResendFlowHandler.handle(keys[0], exchange, chain));
-        handlers.put(jwtUtil.getResetCode(), (exchange, chain, keys) -> emailResetPasswordFlowHandler.handle(keys[0], exchange, chain));
-        handlers.put(jwtUtil.getCodeToSet(), (exchange, chain, keys) -> setNewPasswordFlowHandler.handle(keys[0], exchange, chain));
+        handlers.put(jwtUtil.getHeader(), tokenAuthenticationFlowHandler::handle);
+        handlers.put(jwtUtil.getActivation(), (exchange, chain) -> activationCodeFlowHandler.handle(getKeyFromRequest(exchange, jwtUtil.getActivation()), exchange, chain));
+        handlers.put(jwtUtil.getResend(), (exchange, chain) -> emailResendFlowHandler.handle(getKeyFromRequest(exchange, jwtUtil.getResend()), exchange, chain));
+        handlers.put(jwtUtil.getResetCode(), (exchange, chain) -> emailResetPasswordFlowHandler.handle(getKeyFromRequest(exchange, jwtUtil.getResetCode()), exchange, chain));
+        handlers.put(jwtUtil.getCodeToSet(), (exchange, chain) -> setNewPasswordFlowHandler.handle(getKeyFromRequest(exchange, jwtUtil.getCodeToSet()), exchange, chain));
     }
 
-    @SuppressWarnings("NullableProblems")
-    @Override
+    @SuppressWarnings("NullableProblems") @Override
     public Mono<Void> filter(@NonNull ServerWebExchange exchange, @NonNull WebFilterChain chain) {
+        return handlers.entrySet().stream()
+                .filter(entry -> requestContainsKey(exchange, entry.getKey()))
+                .findFirst()
+                .map(entry -> entry.getValue().handle(exchange, chain))
+                .orElseGet(() -> chain.filter(exchange));
+    }
 
-        MultiValueMap<String, String> queryParams = exchange.getRequest().getQueryParams();
+    private boolean requestContainsKey(ServerWebExchange exchange, String key) {
+        return exchange.getRequest().getHeaders().containsKey(key) ||
+                exchange.getRequest().getQueryParams().containsKey(key);
+    }
 
-        for (Map.Entry<String, RequestHandler> entry : handlers.entrySet()) {
-            if (exchange.getRequest().getHeaders().containsKey(entry.getKey())
-                    || queryParams.containsKey(entry.getKey())) {
-
-                String[] key = queryParams.containsKey(entry.getKey())
-                        ? new String[]{queryParams.getFirst(entry.getKey())} : new String[]{};
-                return entry.getValue().handle(exchange, chain, key);
-            }
-        }
-
-        return chain.filter(exchange);
+    private String getKeyFromRequest(ServerWebExchange exchange, String key) {
+        return exchange.getRequest().getQueryParams().getFirst(key);
     }
 
     @FunctionalInterface
     public interface RequestHandler {
-        Mono<Void> handle(ServerWebExchange exchange, WebFilterChain chain, String... keys);
+        Mono<Void> handle(ServerWebExchange exchange, WebFilterChain chain);
     }
 
 }
