bdf4bf5b46d5772ec9c7c086cb6b0b580810c2ea
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-06-04 12:13:23 +0200

    Update ResetPassCodeTest.java

--- WhataCookUsers/src/test/java/com/whatacook/cookers/controler/ResetPassCodeTest.java
+++ WhataCookUsers/src/test/java/com/whatacook/cookers/controler/ResetPassCodeTest.java
@@ -16,6 +16,7 @@ public class ResetPassCodeTest extends BaseTestClass {
 
     @Value("${app.endpoint.reset-pass}")
     private String usersResetPasswordEndpoint;
+    private String htmlToTest;
     private ResetDto resetDto;
     private UserDTO userDTO;
 
@@ -23,15 +24,17 @@ public class ResetPassCodeTest extends BaseTestClass {
     void setUp() {
         userDTO = userDtoBasicOk();
         resetDto = ResetDto.to(userDTO);
+        Mockito.when(resetDao.findByCode(Mockito.anyString())).thenReturn(Mono.just(resetDto));
+        Mockito.when(userDao.findBy_id(Mockito.anyString())).thenReturn(Mono.just(userDTO));
     }
 
     @Test
     void testResetPassCodeNotFound() {
         Mockito.when(resetDao.findByCode(Mockito.anyString())).thenReturn(Mono.empty());
-        String htmlToTest = Htmls.FailReset.get()
+        htmlToTest = Htmls.FailReset.get()
+                .replace("errorDescriptionValue", "Code Not Found")
                 .replace("LOGO_WAC", globalValues.getUrlWacLogoPngSmall())
-                .replace("EMAIL_WAC", globalValues.getMailToWac())
-                .replace("errorDescriptionValue", "Code Not Found");
+                .replace("EMAIL_WAC", globalValues.getMailToWac());
         webTestClient.get().uri(uriBuilder -> uriBuilder.path(usersResetPasswordEndpoint)
                         .queryParam("resetCode", "invalidCode").build())
                 .exchange()
@@ -43,18 +46,28 @@ public class ResetPassCodeTest extends BaseTestClass {
     @Test
     void testResetPassCodeInvalidAccountStatus() {
         userDTO.setAccountStatus(AccountStatus.OFF);
-        Mockito.when(resetDao.findByCode(Mockito.anyString())).thenReturn(Mono.just(resetDto));
-        Mockito.when(userDao.findBy_id(Mockito.anyString())).thenReturn(Mono.just(userDTO));
         String errorMsg = "Account Status Incorrect for this request: " + userDTO.getAccountStatus().getDetails();
-        String htmlToTest = Htmls.FailReset.get()
+        htmlToTest = Htmls.FailReset.get()
+                .replace("errorDescriptionValue", errorMsg)
                 .replace("LOGO_WAC", globalValues.getUrlWacLogoPngSmall())
-                .replace("EMAIL_WAC", globalValues.getMailToWac())
-                .replace("errorDescriptionValue", errorMsg);
+                .replace("EMAIL_WAC", globalValues.getMailToWac());
         webTestClient.get().uri(uriBuilder -> uriBuilder.path(usersResetPasswordEndpoint)
                         .queryParam("resetCode", resetDto.getCode()).build())
                 .exchange()
                 .expectStatus().isBadRequest()
                 .expectBody(String.class)
                 .value(html -> assertEquals(htmlToTest, html));
+    }
+
+    @Test
+    void testResetPassCodeExpired() {
+        resetDto.setExpiration(resetDto.getExpiration().minusDays(1));
+        webTestClient.get().uri(uriBuilder -> uriBuilder.path(usersResetPasswordEndpoint)
+                        .queryParam("resetCode", resetDto.getCode()).build())
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .jsonPath("$.success").isEqualTo(false)
+                .jsonPath("$.message").isEqualTo("This Code is Expired");
     }
 }
