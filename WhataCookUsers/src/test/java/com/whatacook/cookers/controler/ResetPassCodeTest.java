dbfe393da89b5d9e8996b80260a9a2317eb668cb
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-06-04 09:37:04 +0200

    Create ResetPassCodeTest.java

---
+++ WhataCookUsers/src/test/java/com/whatacook/cookers/controler/ResetPassCodeTest.java
@@ -0,0 +1,34 @@
+package com.whatacook.cookers.controler;
+
+import com.whatacook.cookers.model.constants.Htmls;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Value;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ResetPassCodeTest extends BaseTestClass {
+
+    @Value("${app.endpoint.reset-pass}")
+    private String usersResetPasswordEndpoint;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testResetPassCodeNotFound() {
+        Mockito.when(resetDao.findByCode(Mockito.anyString())).thenReturn(Mono.empty());
+        String htmlToTest = Htmls.FailReset.get()
+                .replace("LOGO_WAC", globalValues.getUrlWacLogoPngSmall())
+                .replace("EMAIL_WAC", globalValues.getMailToWac());
+        webTestClient.get().uri(uriBuilder -> uriBuilder.path(usersResetPasswordEndpoint)
+                        .queryParam("resetCode", "invalidCode").build())
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(String.class)
+                .value(html -> assertEquals(htmlToTest, html));
+    }
+}
