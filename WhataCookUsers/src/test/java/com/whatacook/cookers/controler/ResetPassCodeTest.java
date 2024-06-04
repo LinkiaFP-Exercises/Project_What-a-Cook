68ba6c5ce1cd54790345c1490b939b67138ff4ae
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-06-04 11:55:47 +0200

    implement bad request response

--- WhataCookUsers/src/test/java/com/whatacook/cookers/controler/ResetPassCodeTest.java
+++ WhataCookUsers/src/test/java/com/whatacook/cookers/controler/ResetPassCodeTest.java
@@ -1,6 +1,9 @@
 package com.whatacook.cookers.controler;
 
+import com.whatacook.cookers.model.auth.ResetDto;
+import com.whatacook.cookers.model.constants.AccountStatus;
 import com.whatacook.cookers.model.constants.Htmls;
+import com.whatacook.cookers.model.users.UserDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -13,9 +16,13 @@ public class ResetPassCodeTest extends BaseTestClass {
 
     @Value("${app.endpoint.reset-pass}")
     private String usersResetPasswordEndpoint;
+    private ResetDto resetDto;
+    private UserDTO userDTO;
 
     @BeforeEach
     void setUp() {
+        userDTO = userDtoBasicOk();
+        resetDto = ResetDto.to(userDTO);
     }
 
     @Test
@@ -28,7 +35,25 @@ public class ResetPassCodeTest extends BaseTestClass {
         webTestClient.get().uri(uriBuilder -> uriBuilder.path(usersResetPasswordEndpoint)
                         .queryParam("resetCode", "invalidCode").build())
                 .exchange()
-                .expectStatus().isNotFound()
+                .expectStatus().isBadRequest()
+                .expectBody(String.class)
+                .value(html -> assertEquals(htmlToTest, html));
+    }
+
+    @Test
+    void testResetPassCodeInvalidAccountStatus() {
+        userDTO.setAccountStatus(AccountStatus.OFF);
+        Mockito.when(resetDao.findByCode(Mockito.anyString())).thenReturn(Mono.just(resetDto));
+        Mockito.when(userDao.findBy_id(Mockito.anyString())).thenReturn(Mono.just(userDTO));
+        String errorMsg = "Account Status Incorrect for this request: " + userDTO.getAccountStatus().getDetails();
+        String htmlToTest = Htmls.FailReset.get()
+                .replace("LOGO_WAC", globalValues.getUrlWacLogoPngSmall())
+                .replace("EMAIL_WAC", globalValues.getMailToWac())
+                .replace("errorDescriptionValue", errorMsg);
+        webTestClient.get().uri(uriBuilder -> uriBuilder.path(usersResetPasswordEndpoint)
+                        .queryParam("resetCode", resetDto.getCode()).build())
+                .exchange()
+                .expectStatus().isBadRequest()
                 .expectBody(String.class)
                 .value(html -> assertEquals(htmlToTest, html));
     }
