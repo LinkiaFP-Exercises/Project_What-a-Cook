7156cd696fa36d549f9a5934c5396102b48bda80
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-06-05 09:40:42 +0200

    initial project

---
+++ WhataCookRecipies/src/test/java/linkia/dam/whatacookrecipies/WhataCookRecipiesApplicationTests.java
@@ -0,0 +1,13 @@
+package linkia.dam.whatacookrecipies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhataCookRecipiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
