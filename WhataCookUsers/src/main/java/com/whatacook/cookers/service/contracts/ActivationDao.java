a2019f8c52b942c66cc839a7578ddb773368f07f
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-06-14 12:33:24 +0200

    Update ActivationDao.java

--- WhataCookUsers/src/main/java/com/whatacook/cookers/service/contracts/ActivationDao.java
+++ WhataCookUsers/src/main/java/com/whatacook/cookers/service/contracts/ActivationDao.java
@@ -5,6 +5,21 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
+/**
+ * Repository interface for accessing activation data from MongoDB.
+ * Extends ReactiveMongoRepository for reactive CRUD operations.
+ * <p>
+ * Methods:
+ * - findByCode(String code): Finds an activation DTO by its code.
+ * <p>
+ * Annotations:
+ * - @Repository: Indicates that this interface is a Spring repository.
+ *
+ * @author <a href="https://about.me/prof.guazina">Fauno Guazina</a>
+ * @see ActivationDto
+ * @see Mono
+ * @see ReactiveMongoRepository
+ */
 @Repository
 public interface ActivationDao extends ReactiveMongoRepository<ActivationDto, String> {
 
