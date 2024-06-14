8b4efe032d3849b9b8abab88a23d6e1f9219226a
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-06-14 10:57:46 +0200

    Update MeasureDao.java

--- WhataCookRecipes/src/main/java/linkia/dam/whatacookrecipes/service/repository/MeasureDao.java
+++ WhataCookRecipes/src/main/java/linkia/dam/whatacookrecipes/service/repository/MeasureDao.java
@@ -5,7 +5,27 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
+/**
+ * Repository interface for managing {@link MeasureDto} entities.
+ * Extends the {@link ReactiveMongoRepository} to provide reactive CRUD operations.
+ * <p>
+ * Method:
+ * - findByNameIgnoreCase(String name): Finds a measure by name, case insensitive.
+ * <p>
+ * Annotations:
+ * - @Repository: Indicates that this interface is a Spring Data Repository.
+ *
+ * @Author <a href="https://about.me/prof.guazina">Fauno Guazina</a>
+ * @see ReactiveMongoRepository
+ */
 @Repository
 public interface MeasureDao extends ReactiveMongoRepository<MeasureDto, String> {
+
+    /**
+     * Finds a measure by name, case insensitive.
+     *
+     * @param name The name of the measure to search for.
+     * @return A {@link Mono} emitting the matching {@link MeasureDto} entity, or empty if not found.
+     */
     Mono<MeasureDto> findByNameIgnoreCase(String name);
 }
