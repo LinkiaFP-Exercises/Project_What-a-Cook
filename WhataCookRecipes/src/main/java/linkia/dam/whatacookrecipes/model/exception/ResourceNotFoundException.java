229857d4eacca914ceb7652d05da52b00121b6a3
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-06-14 10:44:23 +0200

    Update ResourceNotFoundException.java

--- WhataCookRecipes/src/main/java/linkia/dam/whatacookrecipes/model/exception/ResourceNotFoundException.java
+++ WhataCookRecipes/src/main/java/linkia/dam/whatacookrecipes/model/exception/ResourceNotFoundException.java
@@ -3,8 +3,17 @@ package linkia.dam.whatacookrecipes.model.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception thrown when a requested resource is not found.
+ * This exception results in a response with a 404 Not Found HTTP status.
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+    /**
+     * Constructs a new ResourceNotFoundException with the specified detail message.
+     *
+     * @param message the detail message.
+     */
     public ResourceNotFoundException(String message) {
         super(message);
     }
