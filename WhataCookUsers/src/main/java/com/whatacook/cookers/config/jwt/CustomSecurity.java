fd471d063f5e1c3d08933155a092e6da023073d5
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-06-14 12:10:57 +0200

    Update CustomSecurity.java

--- WhataCookUsers/src/main/java/com/whatacook/cookers/config/jwt/CustomSecurity.java
+++ WhataCookUsers/src/main/java/com/whatacook/cookers/config/jwt/CustomSecurity.java
@@ -3,6 +3,18 @@ package com.whatacook.cookers.config.jwt;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+/**
+ * Custom security component for checking user permissions.
+ * Provides a method to check if the authenticated user is either the owner or an admin.
+ * <p>
+ * Annotations:
+ * - @Component: Indicates that this class is a Spring component.
+ * <p>
+ * Methods:
+ * - isSelfOrAdmin(Authentication authentication, String userId, String email): Checks if the authenticated user is the owner or an admin.
+ *
+ * @author <a href="https://about.me/prof.guazina">Fauno Guazina</a>
+ */
 @Component("customSecurity")
 public class CustomSecurity {
 
@@ -10,6 +22,6 @@ public class CustomSecurity {
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
         return (userDetails.getId().equals(userId)) || (userDetails.getEmail().equals(email))
                 || authentication.getAuthorities().stream()
-                        .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"));
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"));
     }
 }
