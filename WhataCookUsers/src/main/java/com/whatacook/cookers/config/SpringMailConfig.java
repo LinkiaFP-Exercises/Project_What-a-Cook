059bf27f3bb4c31842fcc048d0891319d3b865c7
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-06-14 12:00:55 +0200

    Update SpringMailConfig.java

--- WhataCookUsers/src/main/java/com/whatacook/cookers/config/SpringMailConfig.java
+++ WhataCookUsers/src/main/java/com/whatacook/cookers/config/SpringMailConfig.java
@@ -15,18 +15,44 @@ import org.springframework.util.StringUtils;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * Configuration class for setting up the Spring Mail with Gmail.
+ * Reads the email credentials from Docker secrets and initializes the JavaMailSender.
+ * <p>
+ * Annotations:
+ * - @Slf4j: Lombok annotation to generate a logger field.
+ * - @NoArgsConstructor: Generates a no-arguments constructor.
+ * - @Setter: Generates setter methods for all fields.
+ * - @Getter: Generates getter methods for all fields.
+ * - @Configuration: Indicates that this class contains Spring configuration.
+ * <p>
+ * Fields:
+ * - springMailUser: The email user for sending mails.
+ * - gMailAppPass: The application password for the Gmail account.
+ * <p>
+ * Methods:
+ * - init(): Initializes the email credentials by reading Docker secrets.
+ * - getJavaMailSender(): Configures and returns a JavaMailSender instance.
+ *
+ * @author <a href="https://about.me/prof.guazina">Fauno Guazina</a>
+ */
 @Slf4j
 @NoArgsConstructor
-@Setter @Getter
+@Setter
+@Getter
 @Configuration
 public class SpringMailConfig {
 
     private String springMailUser;
     @Getter(AccessLevel.NONE)
     private String gMailAppPass;
+
+    /**
+     * Initializes the email credentials by reading Docker secrets.
+     * Sets the system properties for email user and password.
+     */
     @PostConstruct
     public void init() {
-
         try {
             String USER = "SPRING_MAIL_VALIDATION";
             String APP_PASS = "GMAIL_APP_PASSWORD";
@@ -36,16 +62,22 @@ public class SpringMailConfig {
 
             if (StringUtils.hasText(springMailUser))
                 System.setProperty(USER, springMailUser.trim());
-            log.warn("springMailUser : " + StringUtils.hasText(springMailUser));
+            log.warn("springMailUser : {}", StringUtils.hasText(springMailUser));
 
             if (StringUtils.hasText(gMailAppPass))
                 System.setProperty(APP_PASS, gMailAppPass.trim());
-            log.warn("springMailPass : " + StringUtils.hasText(gMailAppPass));
+            log.warn("springMailPass : {}", StringUtils.hasText(gMailAppPass));
 
-        } catch (IOException e) {  throw new RuntimeException(e); }
-
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    /**
+     * Configures and returns a JavaMailSender instance.
+     *
+     * @return The configured JavaMailSender instance.
+     */
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
@@ -63,5 +95,4 @@ public class SpringMailConfig {
 
         return mailSender;
     }
-
 }
