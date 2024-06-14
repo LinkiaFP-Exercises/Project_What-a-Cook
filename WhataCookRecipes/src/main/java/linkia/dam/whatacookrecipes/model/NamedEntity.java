e822f3faed07f1b46eea94e952734af286795360
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-06-14 10:56:07 +0200

    Update NamedEntity.java

--- WhataCookRecipes/src/main/java/linkia/dam/whatacookrecipes/model/NamedEntity.java
+++ WhataCookRecipes/src/main/java/linkia/dam/whatacookrecipes/model/NamedEntity.java
@@ -6,6 +6,29 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+/**
+ * Base entity class with an ID and a name attribute.
+ * <p>
+ * Annotations:
+ * - @NoArgsConstructor: Generates a no-argument constructor.
+ * - @AllArgsConstructor: Generates an all-arguments constructor.
+ * - @Getter: Generates getter methods for all fields.
+ * - @Setter: Generates setter methods for all fields.
+ * - @ToString: Generates a toString method.
+ * - @EqualsAndHashCode: Generates equals and hashCode methods.
+ * <p>
+ * Fields:
+ * - id: The unique identifier of the entity.
+ * - name: The name of the entity, which is mandatory and indexed uniquely.
+ * <p>
+ * Additional Annotations:
+ * - @Id: Marks the field as the primary key in the MongoDB document.
+ * - @Field: Specifies the field name in the MongoDB document.
+ * - @NotBlank: Validates that the field is not null and not blank.
+ * - @Indexed: Marks the field as indexed, ensuring it is unique in the database.
+ *
+ * @author <a href="https://about.me/prof.guazina">Fauno Guazina</a>
+ */
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -13,10 +36,15 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @ToString
 @EqualsAndHashCode
 public class NamedEntity {
-
+    /**
+     * The unique identifier of the entity.
+     */
     @Id
     protected String id;
 
+    /**
+     * The name of the entity, which is mandatory and indexed uniquely.
+     */
     @Field("name")
     @NotBlank(message = "name is mandatory")
     @Indexed(unique = true)
