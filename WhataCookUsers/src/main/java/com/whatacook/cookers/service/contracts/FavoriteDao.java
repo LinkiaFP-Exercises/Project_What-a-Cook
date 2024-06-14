b24d5c672ffd6e2c36d60025c32bf1c192583725
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-06-14 12:33:21 +0200

    Update FavoriteDao.java

--- WhataCookUsers/src/main/java/com/whatacook/cookers/service/contracts/FavoriteDao.java
+++ WhataCookUsers/src/main/java/com/whatacook/cookers/service/contracts/FavoriteDao.java
@@ -3,5 +3,16 @@ package com.whatacook.cookers.service.contracts;
 import com.whatacook.cookers.model.favorites.FavoriteDto;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
+/**
+ * Repository interface for accessing favorite data from MongoDB.
+ * Extends ReactiveMongoRepository for reactive CRUD operations.
+ * <p>
+ * Annotations:
+ * - None
+ *
+ * @author <a href="https://about.me/prof.guazina">Fauno Guazina</a>
+ * @see FavoriteDto
+ * @see ReactiveMongoRepository
+ */
 public interface FavoriteDao extends ReactiveMongoRepository<FavoriteDto, String> {
 }
